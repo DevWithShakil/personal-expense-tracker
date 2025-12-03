9f22605776e445ac14458e54954134a1dda71e88
DevWithShakil <m.shakil720@gmail.com>
2025-12-04 01:47:50 +0600

    transaction controller implemented

---
+++ app/Http/Controllers/Api/TransactionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\TransactionResource;
+use App\Models\Transaction;
+use App\Http\Requests\StoreTransactionRequest;
+
+
+
+class TransactionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->user()
+                    ->transactions()
+                    ->with('category');
+
+        if($request->hash('month')){
+            $query->whereMonth('transaction_date', $request->month);
+        }
+
+        if($request->hash('year')){
+            $query->whereMonth('transaction_date', $request->year);
+        }
+
+        if($request->hash('type')){
+            $query->where('category', function($q) use ($request){
+                $q->where('type', $request->type);
+            });
+        }
+
+        $transactions = $query->latest('transaction_date')->paginate(20);
+        return TransactionResource::collection($transactions);
+    }
+
+    public function store(StoreTransactionRequest $request)
+    {
+        $transaction = $request->user()->transactions()->create($request->validated());
+        $transaction->load('category');
+        return new TransactionResource($transaction);
+    }
+
+    public function destroy(Transaction $transaction)
+    {
+        if($transaction->user_id !== auth()->user()->id)
+        {
+            abort(403, 'You are not authorized to delete this transaction.');
+        }
+
+        $transaction->delete();
+        return response()->noContent();
+    }
+}
