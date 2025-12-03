f10435f0181098c509419fee351acbf4ea75c793
DevWithShakil <m.shakil720@gmail.com>
2025-12-04 01:25:06 +0600

    category policies and route configuration implemented

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::apiResource('categories', CategoryController::class);
 });
