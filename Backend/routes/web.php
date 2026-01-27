232fdeafe459221ec78149d8a711dc3a8db47af1
DevWithShakil <m.shakil720@gmail.com>
2026-01-27 12:30:47 +0600

    Fix backend route for API

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Backend API is Running Successfully!',
+        'status' => 200
+    ]);
 });
-
 require __DIR__.'/auth.php';
